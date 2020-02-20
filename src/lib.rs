a24b89e15dc1bff2dc4e589197c79b447cec74f3
Jovansonlee Cesar <ivanceras@gmail.com>
2020-02-21 01:29:06 +0800

    Replace the original implementation using the parser code from sauron

--- src/lib.rs
+++ src/lib.rs
@@ -1,294 +1,111 @@
-#![deny(warnings)]
-extern crate html5ever;
+//#![deny(warnings)]
+use sauron::parser::convert_html_to_syntax;
+use sauron::{
+    html::units::*,
+    html::{attributes::*, events::*, *},
+    input, Cmd, Component, Node, Program, *,
+};
+use wasm_bindgen::prelude::*;
 
-use html5ever::driver;
-use html5ever::local_name;
-use html5ever::namespace_url;
-use html5ever::ns;
-use html5ever::parse_fragment;
-use html5ever::parse_document;
-use html5ever::rcdom::Node;
-use html5ever::rcdom::{NodeData, RcDom};
-use html5ever::tendril::TendrilSink;
-use html5ever::QualName;
-use std::fs::File;
-use std::io;
-use std::io::Read;
-use std::io::Write;
-use std::path::PathBuf;
-use structopt::StructOpt;
+#[macro_use]
+extern crate log;
 
-mod attributes;
-mod tags;
-
-/// Initializes an HTML fragment parser.
-///
-fn make_fragment_parser() -> driver::Parser<RcDom> {
-    parse_fragment(
-        RcDom::default(),
-        driver::ParseOpts::default(),
-        QualName::new(None, ns!(html), local_name!("div")),
-        vec![],
-    )
+pub enum Msg {
+    ChangeInput(String),
+    Convert,
+    ToggleMacro,
 }
 
-/// Initializes an HTML document parser
-/// Use this when the supplied string starts with html
-fn make_document_parser() -> driver::Parser<RcDom> {
-    parse_document(
-        RcDom::default(),
-        driver::ParseOpts::default(),
-    )
+pub struct App {
+    input: String,
+    output: String,
+    use_macro: bool,
 }
 
-fn padd(n: i32) -> String {
-    let mut buffer = String::new();
-    for _ in 0..n {
-        buffer += "    ";
+impl App {
+    pub fn new() -> Self {
+        App {
+            input: String::new(),
+            output: String::new(),
+            use_macro: false,
+        }
     }
-    buffer
 }
 
-fn process_element(elm: &NodeData, children_str: &str, indent: i32, opt: &Opt) -> Option<String> {
-    match elm {
-        NodeData::Element { name, attrs, .. } => {
-            let tag = name.local.to_string();
-            if opt.strip_script && tag == "script" {
-                None
-            } else {
-                let corrected_tag = if tags::is_valid_tag(&tag) {
-                    tag
-                } else {
-                    // replace custom tag with div
-                    "div".to_string()
-                };
-                let mut elm_buffer = String::new();
-                elm_buffer += &format!("{}([", corrected_tag);
-                let mut att_buffer = vec![];
-                for att in attrs.borrow().iter() {
-                    let key = att.name.local.to_string();
-                    let value = att.value.to_string();
-                    if opt.strip_invalid && !attributes::is_valid(&key) {
-                        // exclude
-                    } else {
-                        let attrib = attributes::format(&key, &value, opt);
-                        if !attrib.is_empty() {
-                            att_buffer.push(attrib);
-                        }
-                    }
-                }
-                elm_buffer += &att_buffer.join(", ");
-                elm_buffer += &format!("],[\n");
-                elm_buffer += &format!("{}{}", padd(indent), children_str,);
-                elm_buffer += &format!("\n{}])", padd(indent - 1));
-                Some(elm_buffer)
+impl Component<Msg> for App {
+    fn view(&self) -> Node<Msg> {
+        div!(
+            [styles([("display", "flex"), ("flex-direction", "column")])],
+            [
+                label!([for_("html_input")], [text("Paste your html here:")]),
+                textarea!(
+                    [
+                        id("html_input"),
+                        styles([("height", px(400)),]),
+                        oninput(|input| Msg::ChangeInput(input.value)),
+                        value(&self.input),
+                    ],
+                    []
+                ),
+                div!(
+                    [styles([("display", "flex"), ("flex-direction", "row")])],
+                    [
+                        button!(
+                            [styles([("width", px(200))]), onclick(|_| Msg::Convert)],
+                            [text("Convert >> ")]
+                        ),
+                        input!(
+                            [
+                                type_("checkbox"),
+                                id("use_macro_check"),
+                                onclick(|_| Msg::ToggleMacro)
+                            ],
+                            []
+                        )
+                        .add_attributes(attrs_flag([(
+                            "checked",
+                            "checked",
+                            self.use_macro
+                        )])),
+                        label!([for_("use_macro_check")], [text("Use macro")])
+                    ]
+                ),
+                label!([for_("sauron_syntax")], [text("Sauron view code")]),
+                textarea!(
+                    [
+                        id("sauron_syntax"),
+                        styles([("height", px(400))]),
+                        value(&self.output)
+                    ],
+                    []
+                ),
+            ]
+        )
+    }
+
+    fn update(&mut self, msg: Msg) -> Cmd<Self, Msg> {
+        match msg {
+            Msg::ChangeInput(input) => {
+                self.input = input.clone();
+                /*
+                self.output = convert_html_to_syntax(&input, self.use_macro);
+                log::debug!("self.output: {}", self.output);
+                */
+            }
+            Msg::Convert => {
+                self.output = convert_html_to_syntax(&self.input, self.use_macro);
+            }
+            Msg::ToggleMacro => {
+                self.use_macro = !self.use_macro;
+                self.output = convert_html_to_syntax(&self.input, self.use_macro);
             }
         }
-
-        NodeData::Text { contents } => {
-            let text = contents.borrow();
-            if !text.trim().is_empty() {
-                let text = text.trim().to_string();
-                // use raw formatting when there are quotes inside text
-                if text.contains("\""){
-                    Some(format!("text(r#\"{}\"#)", text))
-                }else{
-                    Some(format!("text(\"{}\")", text))
-                }
-            } else {
-                None
-            }
-        }
-        _elm => {
-            if children_str.trim().is_empty() {
-                None
-            } else {
-                Some(children_str.to_string())
-            }
-        }
+        Cmd::none()
     }
 }
 
-fn process_node(node: &Node, indent: i32, opt: &Opt) -> Option<String> {
-    // process the children first
-    let mut child_buffer = vec![];
-    for child in node.children.borrow().iter() {
-        if let Some(child_elm) = process_node(child, indent + 1, opt) {
-            child_buffer.push(child_elm);
-        }
-    }
-    let children_str = child_buffer.join(&format!(",\n{}", padd(indent)));
-    process_element(&node.data, &children_str, indent, opt)
-}
-
-#[derive(StructOpt, Debug, Default)]
-#[structopt(name = "html2sauron")]
-pub struct Opt {
-    /// Output file
-    #[structopt(short = "o", long = "output", parse(from_os_str))]
-    pub output: Option<PathBuf>,
-
-    /// Trim invalid attributes
-    #[structopt(
-        long = "strip_invalid_attributes",
-        parse(try_from_str),
-        default_value = "true"
-    )]
-    strip_invalid: bool,
-
-    /// Files to process
-    #[structopt(name = "FILE", parse(from_os_str))]
-    pub file: PathBuf,
-
-    /// Remove classes that is prefix with argument
-    #[structopt(short = "r", long = "strip-classes-with-prefix")]
-    strip_class_with_prefix: Option<String>,
-
-    /// Trim invalid attributes
-    #[structopt(long = "strip_script", parse(try_from_str), default_value = "true")]
-    strip_script: bool,
-}
-
-pub fn read_file(file: &PathBuf) -> io::Result<String> {
-    let mut file = File::open(file)?;
-    let mut buffer = String::new();
-    file.read_to_string(&mut buffer)?;
-    Ok(buffer)
-}
-
-pub fn write_to_file(file: &PathBuf, sauron: &str) -> io::Result<()> {
-    let mut file = File::create(file)?;
-    file.write_all(sauron.as_bytes())?;
-    Ok(())
-}
-
-pub fn html2sauron(html: &str, opt: &Opt) -> String {
-    let html_start = html.trim_start();
-    let parser = if html_start.starts_with("<html") || html_start.starts_with("<!DOCTYPE"){
-        make_document_parser()
-    }else{
-        make_fragment_parser()
-    };
-    let fragment = parser.one(html);
-    if let Some(code) = process_node(&fragment.document, 0, opt) {
-        code
-    } else {
-        "".to_string()
-    }
-}
-
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test1() {
-        let html = "<a>Hello world</a><div class='btn large'>link to here</div><utag>how to deal with this</utag>";
-        let view = html2sauron(html, &Opt::default());
-        println!("{}", view);
-        assert_eq!(
-            view,
-            r#"
-html([],[
-    a([],[
-        text("Hello world")
-    ]),
-    div([class("btn large")],[
-        text("link to here")
-    ]),
-    div([],[
-        text("how to deal with this")
-    ])
-])
-                   "#
-            .trim()
-            .to_string()
-        );
-    }
-
-    #[test]
-    #[should_panic]
-    //FIXME: need to trim trailing spaces
-    fn test_html_document() {
-        let html = "<html><body><a>Hello world</a><div class='btn large'>link to here</div><utag>how to deal with this</utag></body></html>";
-        let view = html2sauron(html, &Opt::default());
-        println!("{}", view);
-        assert_eq!(
-            view,
-r#"
-html([],[
-    head([],[
-
-    ]),
-    body([],[
-        a([],[
-            text("Hello world")
-        ]),
-        div([class("btn large")],[
-            text("link to here")
-        ]),
-        div([],[
-            text("how to deal with this")
-        ])
-    ])
-])
-"#
-            .trim()
-            .to_string()
-        );
-    }
-
-    #[test]
-    fn test_replace_ng() {
-        let html = "<a>Hello world</a><div class='btn large ng-scope'>link to here</div><utag>how to deal with this</utag>";
-        let mut opt = Opt::default();
-        opt.strip_class_with_prefix = Some("ng-".to_string());
-        let view = html2sauron(html, &opt);
-        println!("{}", view);
-        assert_eq!(
-            view,
-            r#"
-html([],[
-    a([],[
-        text("Hello world")
-    ]),
-    div([class("btn large")],[
-        text("link to here")
-    ]),
-    div([],[
-        text("how to deal with this")
-    ])
-])
-                   "#
-            .trim()
-            .to_string()
-        );
-    }
-
-    #[test]
-    #[should_panic]
-    fn test_with_invalid_tag() {
-        let html = "<a>Hello world</a><div class='btn large'>link to here</div><utag>how to deal with this</utag>";
-        let view = html2sauron(html, &Opt::default());
-        println!("{}", view);
-        assert_eq!(
-            view,
-            r#"
-html([],[
-    a([],[
-        text("Hello world")
-    ]),
-    div([class("btn large")],[
-        text("link to here")
-    ]),
-    utag([],[
-        text("how to deal with this")
-    ])
-])
-                   "#
-            .trim()
-            .to_string()
-        );
-    }
+#[wasm_bindgen(start)]
+pub fn main() {
+    console_log::init_with_level(log::Level::Trace).expect("must be initiated");
+    Program::mount_to_body(App::new());
 }
